66e2aa8dff072a1f13bf658f1f1346a6ef126395
jyyblue <jyyblue1987@outlook.com>
2022-06-02 18:16:00 +0800

    [add] add springer journal downloader.

---
+++ springer.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+from time import sleep
+from sci_hub_pdf import download_pdf
+
+
+JOURNAL_BASE_URL = 'https://link.springer.com'
+JOURNAL_URL = f"{JOURNAL_BASE_URL}/journal/10107/volumes-and-issues"
+response = requests.get(JOURNAL_URL)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+current_dir = os.getcwd()
+output_folder = current_dir + "/pdfs"
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+
+# print(soup)
+for link in soup.select('li.app-section .c-list-group__item a'):
+    href = link['href']
+    if "154" in href:
+        break 
+
+    href = JOURNAL_BASE_URL + href
+    print(href)
+
+    sleep(0.1)
+
+    response = requests.get(href)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    title = soup.select_one('.app-volumes-and-issues__info h1').contents[0]
+    print(title)
+
+    output_folder = current_dir + "/pdfs/" + title
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    sleep(0.1)
+
+    for link in soup.select('h3.c-card__title a'):
+        href = link['href']
+        title = link.contents[0]
+
+        # get doi
+        doi = href.replace("https://link.springer.com/article/", "")
+        print(doi, title)
+
+        download_pdf(doi, output_folder, title)
+
+    break
+
+
+    
