fe1a7ba278b4ebda7bf4e8df3f1e63f30a1f87da
jyyblue <jyyblue1987@outlook.com>
2023-02-13 19:22:39 +0800

    [add] check linked none

--- codementor/codementor.py
+++ codementor/codementor.py
@@ -22,12 +22,19 @@ for x in data['hits']:
     soup = BeautifulSoup(response.content, 'html.parser')
 
     link = soup.select_one('.linkedin-url')
+    if link is None:
+        print(f"{i + 1}/{count}", x["username"], "There is no linkedin")  
+        i += 1
+        continue  
+
     href = link["href"]
     print(f"{i + 1}/{count}", x["username"], href)
     x["linkedin"] = href
 
     result.append(x)
     # break
+
+    i += 1
 
 
 df = pd.DataFrame.from_dict(result)
